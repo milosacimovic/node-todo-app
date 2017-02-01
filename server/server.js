32371aa88afce2f9fe7bc0349cbf2eca57984fe8
milosacimovic <milosacimovic@hotmail.com>
2017-02-01 20:44:44 +0100

    Tested GET /todos/:id API

--- server/server.js
+++ server/server.js
@@ -37,18 +37,13 @@ app.get('/todos/:id', (req, res) => {
   }
   Todo.findById(id).then( todo => {
     if(!todo){
+      console.log(todo);
       return res.status(404).send();
     }
     res.send({todo});
   }).catch( e => {
     res.status(400).send();
   });
-  //findById
-   //success case
-    //if todo - send it back
-    //if no todo - send back 404 with empty body
-   //error
-    //400 - and send empty body back
 });
 
 app.listen(3000, () => {
