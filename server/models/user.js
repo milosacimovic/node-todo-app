f47b898c176cb4ba3865012ac9ec8bda68b9570a
milosacimovic <milosacimovic@hotmail.com>
2017-02-03 22:25:26 +0100

    Add bcryptjs password hashing

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 var validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
 var UserSchema = new mongoose.Schema({
   email: {
     required: true,
@@ -64,6 +66,21 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.access': 'auth'
   });
 }
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if(user.isModified('password')){
+    //user.password hashing and salting
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  }else{
+    next();
+  }
+});
 var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
